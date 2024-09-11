87104b67eeac6823fa3a66122f330963025f74b3
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-10 21:46:24 -0400

    commented out game blueprint, and also change the path of my config to match location

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
-from .config import Config
+from config import Config
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -16,8 +17,9 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    from .routes import auth, game
+    from .routes import auth #,game
     app.register_blueprint(auth.bp)
-    app.register_blueprint(game.bp)
+    # app.register_blueprint(game.bp)
+
 
     return app
