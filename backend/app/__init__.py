f4216dc51ed890b4e912174f13a8db94e4df1890
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-12 00:11:18 -0400

    added flask-cors for frontend -> backend requests

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
@@ -13,12 +14,14 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    from .routes import auth #,game
+    from .routes import auth, dashboard #,game
     app.register_blueprint(auth.bp, url_prefix='/auth')
+    app.register_blueprint(dashboard.bp)
     # app.register_blueprint(game.bp)
 
 
