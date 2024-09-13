94b26dfe867339f273f69009dbeb08261df7db5e
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-13 12:33:57 -0400

    CORS, and register blueprint added

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,17 +12,16 @@ jwt = JWTManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(config_class)
-
     CORS(app)
+    app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    from .routes import auth, dashboard #,game
+    from .routes import auth, dashboard 
     app.register_blueprint(auth.bp, url_prefix='/auth')
     app.register_blueprint(dashboard.bp)
-    # app.register_blueprint(game.bp)
+
 
 
     return app
