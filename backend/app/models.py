61a9882f15a2222117b7d6aa184e61c5db1d22a7
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-14 15:59:12 -0400

    removed tables from model

--- backend/app/models.py
+++ backend/app/models.py
@@ -17,16 +17,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)  # make sure the username is unique with an index
     password_hash = db.Column(db.String(256)) 
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc) ) 
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc)) 
     
     # user stats
     games_played = db.Column(db.Integer, default=0)
     total_score = db.Column(db.Integer, default=0)
     highest_score = db.Column(db.Integer, default=0)
-
-    # the relationships to other models
-    challenges = db.relationship('Challenge', backref='creator', lazy=True)  # challenges created by this user
-    responses = db.relationship('ChallengeResponse', backref='user', lazy=True)  # responses given by this user
     
     # usernames are unique
     __table_args__ = (UniqueConstraint('username', name='_username_uc'),)
@@ -60,31 +56,6 @@ class DifficultyEnum(Enum):
     MEDIUM = 'medium'
     HARD = 'hard'
 
-class Challenge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    theme = db.Column(db.String(64))
-    difficulty = db.Column(SqlEnum(DifficultyEnum), nullable=False)  # use enumeration for difficulty
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-    expires_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc) + timedelta(days=7))
-
-    responses = db.relationship('ChallengeResponse', backref='challenge', lazy=True)
-    questions = db.relationship('ChallengeQuestion', backref='challenge', lazy=True)
-
-    def __repr__(self):
-        return f'<Challenge {self.id} by User {self.creator_id}>'
-
-class ChallengeResponse(db.Model):
-    # challenge response model: represents a user's response to a challenge
-    id = db.Column(db.Integer, primary_key=True)
-    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    score = db.Column(db.Integer)
-    completed_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
-
-    def __repr__(self):
-        return f'<ChallengeResponse {self.id} by User {self.user_id} for Challenge {self.challenge_id}>'
-
 class PowerUp(db.Model):
     __tablename__ = 'powerup'
     id = db.Column(db.Integer, primary_key=True)
@@ -95,7 +66,6 @@ class PowerUp(db.Model):
         return f'<PowerUp {self.name}>'
 
 class Question(db.Model):
-    # question model: represents a question used in challenges
     id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.String(255), nullable=False)
     correct_answer = db.Column(db.String(255), nullable=False)
@@ -105,16 +75,5 @@ class Question(db.Model):
     option_c = db.Column(db.String(255))
     option_d = db.Column(db.String(255))
 
-    challenge_questions = db.relationship('ChallengeQuestion', backref='question', lazy=True)
-
     def __repr__(self):
         return f'<Question {self.id}: {self.question_text[:20]}...>'
-
-class ChallengeQuestion(db.Model):
-    # challenge question model: links questions to challenges
-    id = db.Column(db.Integer, primary_key=True)
-    challenge_id = db.Column(db.Integer, db.ForeignKey('challenge.id'), nullable=False)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<ChallengeQuestion {self.id} for Challenge {self.challenge_id}>'
