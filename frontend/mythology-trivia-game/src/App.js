42bb99009f16c8e1314e6d777a3110be08850d7a
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-14 13:55:12 -0400

    added first gameboard and question set

--- frontend/mythology-trivia-game/src/App.js
+++ frontend/mythology-trivia-game/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/Private';
+import Gameboard from './components/Gameboard';
+import NorseBoard from './components/Gameboards/Norse';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/gameboard" element={<Gameboard />} />
+          <Route path="/gameboard/norse" element={<NorseBoard />} />
+        </Route>
+      </Routes>
+    </Router>
+  )
+};
 
 export default App;
+
