9bd7a1bc8cbb548ec1bf83c5dc975d9872cb58a4
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-14 13:54:49 -0400

    added game bp, as well as configured cors w access token

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
 from flask_cors import CORS
+from dotenv import load_dotenv
+from datetime import timedelta
 
+load_dotenv()
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,16 +16,24 @@ jwt = JWTManager()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}},
+            supports_credentials=True, allow_headers=["Content-Type", "Authorization"])
+
+
     app.config.from_object(config_class)
+    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)
+    
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
-
-    from .routes import auth, dashboard 
+    
+    from .routes import auth, dashboard, profile, game
     app.register_blueprint(auth.bp, url_prefix='/auth')
     app.register_blueprint(dashboard.bp)
-
-
-
+    app.register_blueprint(profile.bp, url_prefix='/profile')
+    app.register_blueprint(game.bp)
+    
     return app
+
+
+
