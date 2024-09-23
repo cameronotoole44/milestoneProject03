95fde8455aeeec6ea6f00fd917f1b848bc5a467a
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-23 16:04:38 -0400

    update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -15,29 +15,28 @@ migrate = Migrate()
 jwt = JWTManager()
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
-    CORS(app, 
-         
-     resources={r"/*": {"origins":[ "http:/localhost:3000", "https://milestone-project03.vercel.app/"]}},
-     supports_credentials=True,
-     allow_headers=["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
-     expose_headers=["Content-Type", "Authorization"])
+    application = Flask(__name__)
+    CORS(application,
+         resources={r"/*": {"origins": [
+             "http://localhost:3000",
+             "https://milestone-project03.vercel.app"
+         ]}},
+         supports_credentials=True,
+         allow_headers=["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
+         expose_headers=["Content-Type", "Authorization"]
+    )
 
+    application.config.from_object(config_class)
+    application.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2)
 
-    app.config.from_object(config_class)
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2) 
-    
-    db.init_app(app)
-    migrate.init_app(app, db)
-    jwt.init_app(app)
-    
+    db.init_app(application)
+    migrate.init_app(application, db)
+    jwt.init_app(application)
+
     from .routes import auth, dashboard, profile, game
-    app.register_blueprint(auth.bp, url_prefix='/auth')
-    app.register_blueprint(dashboard.bp)
-    app.register_blueprint(profile.bp)
-    app.register_blueprint(game.bp)
-    
-    return app
+    application.register_blueprint(auth.bp, url_prefix='/auth')
+    application.register_blueprint(dashboard.bp)
+    application.register_blueprint(profile.bp)
+    application.register_blueprint(game.bp)
 
-
-
+    return application
