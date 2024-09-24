9a35c6ad29454737846dcf1e74457026dd47c1f5
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-24 11:52:40 -0400

    req.txt fix

---
+++ backend/application.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, send_from_directory
+
+application = Flask(__name__, static_folder='static')
+
+@application.route('/', defaults={'path': ''})
+@application.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(application.static_folder, path)):
+        return send_from_directory(application.static_folder, path)
+    else:
+        return send_from_directory(application.static_folder, 'index.html')
