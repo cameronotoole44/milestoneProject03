952d3bf7326676844797f9886acaca07852ce672
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-24 10:59:31 -0400

    documentation

--- backend/app.py
+++ backend/app.py
@@ -6,10 +6,7 @@ app = Flask(__name__, static_folder='static')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
