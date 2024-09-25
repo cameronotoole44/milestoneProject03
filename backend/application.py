0b2fc32999001b3a08965c49be8351bd3cc05d10
cameronotoole44 <cameronotoole404@gmail.com>
2024-09-25 10:45:26 -0400

    port update

--- backend/application.py
+++ backend/application.py
@@ -12,3 +12,6 @@ def serve(path):
         return send_from_directory(application.static_folder, path)
     else:
         return send_from_directory(application.static_folder, 'index.html')
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=int(os.getenv("PORT", 8080)))
